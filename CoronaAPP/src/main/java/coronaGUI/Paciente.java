f2d80b5adcbc34030f8382a13a5a00fc26adf94d
gatofeles <alexofeles@gmail.com>
2021-05-01 18:17:16 -0300

    add Pessoa

---
+++ CoronaAPP/src/main/java/coronaGUI/Paciente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coronaGUI;
+
+/**
+ *
+ * @author gatofeles
+ */
+public class Paciente extends Pessoa {
+    private String[] sintomas = new String();
+}
--- CoronaAPP/src/main/java/coronaGUI/Pessoa.java
+++ CoronaAPP/src/main/java/coronaGUI/Pessoa.java
@@ -11,4 +11,82 @@ package coronaGUI;
  */
 public class Pessoa {
     
+    //Propriedades
+    protected String nome = "";
+    protected int telefone;
+    protected int cpf;
+    protected String cidade = "";
+    protected String estado = "";
+    protected int id;
+    private String senha = "";
+    
+    //Getters & Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+      public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    //Métodos
+    
+    public void acessarMensagens(){
+        
+    }
+
+
+  
+    
+    
+    
 }
